957c68d9ecba113e58e9c82142132eed51093564
Manthankumar Satani <satanimanthan@gmail.com>
2025-09-11 17:51:13 +0530

    feat: default swap to panora

--- packages/sdk/src/composer-strategies/protocols/default-swap.ts
+++ packages/sdk/src/composer-strategies/protocols/default-swap.ts
@@ -1,4 +1,4 @@
 // replace one swap with the other only one type of swap is allowed at a time
-export * from './dex_swap' // hyperion or thala v2 (based on pool availability)
+// export * from './dex_swap' // hyperion or thala v2 (based on pool availability)
 
-// export { swap } from './panora'
+export * from './panora'
